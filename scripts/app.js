443f9d667848cbff3c77a55e3a637736e6105fb7
Wycliffe Obia <obiawycliffe@outlook.com>
2020-07-09 07:49:44 +0300

    Added Some Quotes

--- scripts/app.js
+++ scripts/app.js
@@ -23,15 +23,37 @@ const quotes = [
       "Repeat it until you learn. That's all it takes :)",
     author: "Obi Wyc",
   },
+  {
+    quote:
+      "If it hurts, do it more often.",
+    author: "Martin Fowler",
+  },
+  {
+    quote:
+      "Happiness has much more to do with how we think about the world than the world itself.",
+    author: "Anonymous",
+  },
+  {
+    quote:
+      "Everyone is trying to figure out life. You don't see it coz our interactions are a game of confidence.",
+    author: "Anonymous",
+  },
+  {
+    quote:
+      "Failure to plan is planning for failure. Always draft something down :)",
+    author: "Obi Wyc",
+  },
+  {
+    quote:
+      "You will be much more in control, if uiu realise how much you are not in control.",
+    author: "Benjamin Graham",
+  },
 ];
 function generateQuote() {  
   var randomIndex = Math.floor(Math.random()*quotes.length);  
  document.getElementById("quote").innerHTML=quotes[randomIndex].quote;
   document.getElementById("author").innerHTML="~"+ " " + quotes[randomIndex].author;
   
-  // Hides Button Element to After Click
-  document.getElementById("launch").style.display='none';
-  
-  //Displays New Button Text After Click
-  document.getElementById("generate").style.display='block';
+  // Change Button Text After Click
+  document.getElementById("generate").innerHTML="New Quote";
 }
